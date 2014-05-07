cdfd645d4eca8e5e811010e26e003f6ab36f61ef
CODeRUS <coderusinbox@gmail.com>
2014-05-07 22:16:59 +0600

    Remove privileged

--- daemon/main.cpp
+++ daemon/main.cpp
@@ -3,16 +3,9 @@
 
 #include "proximitysocket.h"
 
-#include <sys/types.h>
-#include <grp.h>
-#include <pwd.h>
-
 Q_DECL_EXPORT
 int main(int argc, char *argv[])
 {
-    setuid(getpwnam("nemo")->pw_uid);
-    setgid(getgrnam("privileged")->gr_gid);
-
     QScopedPointer<QGuiApplication> app(SailfishApp::application(argc, argv));
     app->setOrganizationName("coderus");
     app->setApplicationName("proximity-speaker");
